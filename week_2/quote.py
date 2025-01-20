45696daa03b161ead346e7059aeec4501f199918
racwhi <128566218+racwhi@users.noreply.github.com>
2025-01-19 21:47:10 -0600

    quote printer assignment

---
+++ week_2/quote.py
@@ -0,0 +1,17 @@
+"""
+Author: Rachel White
+Date: 1/19/2025
+File Name: quote.py
+Description: This program prints a quote and author of quote.
+"""
+
+#  function to display quote along with the author name
+def print_quote():
+    quote = "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character."
+    author = "Martin Luther King Jr."
+    
+    # Printing the quote and the author
+    print(f'"{quote}" - {author}')
+
+# Calling the function to print quote
+print_quote() 
